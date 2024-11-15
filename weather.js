0e0eae08a9284f798ce12019924d37dd7adbdbfb
Abu2013 <tesfayewalelign44@gmail.com>
2024-11-15 15:17:22 -0500

    Updated feature XYZ

--- weather.js
+++ weather.js
@@ -1,2 +1,16 @@
-var messageVar="hello";
-console.log(messageVar);
+const apikey="dd6704a7f8dd5668c39380ece4c55d0c";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?q=London";
+
+
+
+
+
+
+async function weather(){
+    
+    const response = await fetch(apiurl + '$appid=${apikey}');
+    var data =await response.json();
+
+    console.log(data);
+}
+  weather();
